698c653ff85ad56f6dd53b31412878dc65a46071
bala9248 <balapranesh99@gmail.com>
2022-03-04 20:40:03 -0700

    Assignment-7-Part-2-Completed

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -5,7 +5,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 7 git contents
-LDD_VERSION = 448365ae733593aa1ff5973d20a6a95551e0980a
+LDD_VERSION = eac3af879397e4782c5c54966350ebac325bb434
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -14,8 +14,8 @@ LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-bala9248.git
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
 
-LDD_MODULE_SUBDIRS = misc-modules/
-LDD_MODULE_SUBDIRS += scull/
+LDD_MODULE_SUBDIRS = misc-modules
+LDD_MODULE_SUBDIRS += scull
 
 # Install misc-modules and scull components
 define LDD_INSTALL_TARGET_CMDS
